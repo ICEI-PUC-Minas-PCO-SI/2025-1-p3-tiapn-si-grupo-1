bf9f3e274029c0c2cfbc202ab85f7415b8d9612b
RogerDLima <rogeriolimadias@gmail.com>
2025-06-01 16:27:16 -0300

    Add Curtida Flow Salvo Controllers

---
+++ src/back/src/routes/flowsalvoRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const flowSalvoController = require('../controllers/flowsalvoController');
+const auth = require('../middlewares/auth');
+
+router.post('/', flowSalvoController.criar);
+router.get('/', flowSalvoController.listar);
+router.get('/:id', flowSalvoController.obter);
+router.delete('/:id', flowSalvoController.deletar);
+
+module.exports = router;
