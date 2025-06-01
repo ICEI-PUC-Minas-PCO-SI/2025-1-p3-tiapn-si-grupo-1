f6f230fdd3dfefa6a09a2d12698274b15b802a1d
Victor Alves <victor.alvessalcantara@gmail.com>
2025-05-31 23:18:34 -0300

    Adicionando branch para implementação do Feed

---
+++ src/front/src/components/SearchBar/index.jsx
@@ -0,0 +1,28 @@
+import { Search } from "lucide-react";
+import { useState } from "react";
+import { Form, Input, IconWrapper, InputWrapper } from "./style";
+
+//31/05 - UPDATES
+//- Barra de pesquisa já adicionada
+//- valor de busca já mapeado
+//- Resta apenas relacionar as pesquisas com os flow criados
+
+export default function SearchBar() {
+  const [description, setDescription] = useState("");
+
+  return (
+    <Form onSubmit={(e) => setDescription(e.target.value)}>
+      <InputWrapper>
+        <IconWrapper>
+          <Search size={20} />
+        </IconWrapper>
+        <Input
+          type="text"
+          placeholder="Buscar Flow por título, tag ou autor..."
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        ></Input>
+      </InputWrapper>
+    </Form>
+  );
+}
--- src/front/src/routes/index.jsx
+++ src/front/src/routes/index.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Landing from '../pages/Auth/Landing';
-import FlowRoutes from './FlowRoutes';
-import AuthRoutes from './AuthRoutes';
-import CommunityRoutes from './CommunityRoutes';
+import Landing from "../pages/Auth/Landing";
+import FlowRoutes from "./FlowRoutes";
+import AuthRoutes from "./AuthRoutes";
+import CommunityRoutes from "./CommunityRoutes";
+import FeedRoutes from "./FeedRoutes";
 
 const MainRoutes = () => (
   <Routes>
     {/* Página inicial pública */}
     <Route path="/" element={<Landing />} />
-    
+
     {/* Rotas públicas */}
     {AuthRoutes()}
 
     {/* Rotas autenticadas */}
     {FlowRoutes()}
     {CommunityRoutes()}
+    {FeedRoutes()}
   </Routes>
 );
 
