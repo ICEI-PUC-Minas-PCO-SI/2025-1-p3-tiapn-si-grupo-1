2ca8abaddcc34340bfbea9e805c5613afbd5af0f
Gabriella Paz <124802846+gabpazz@users.noreply.github.com>
2025-06-01 18:43:48 -0300

    Adicionando Postagem comunidade Routes

--- src/back/src/routes/postagemComunidadeRoutes.js
+++ src/back/src/routes/postagemComunidadeRoutes.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express'); 
 const router = express.Router();
 const postagemController = require('../controllers/postagemComunidadeController');
@@ -8,4 +9,16 @@ router.get('/postagem/:id', postagemController.buscarPorId);
 router.put('/postagem/:id', postagemController.atualizar);
 router.delete('/postagem/:id', postagemController.deletar);
 
+=======
+const express = require('express'); 
+const router = express.Router();
+const postagemController = require('../controllers/postagemComunidadeController');
+
+router.post('/postagem', postagemController.criar); 
+router.get('/postagem', postagemController.listarTodas);
+router.get('/postagem/:id', postagemController.buscarPorId);
+router.put('/postagem/:id', postagemController.atualizar);
+router.delete('/postagem/:id', postagemController.deletar);
+
+>>>>>>> 338ea9e (Adicionando Postagem comunidade Routes)
 module.exports = router; 
