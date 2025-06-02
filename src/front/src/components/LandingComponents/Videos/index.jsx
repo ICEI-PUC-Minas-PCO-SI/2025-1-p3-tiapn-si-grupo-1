08b546876f82bf824456c1e0091b8ceb77981291
Lucas <borgesbfmg10@gmail.com>
2025-06-01 23:10:02 -0300

    Implementando biblioteca plyr

--- src/front/src/components/LandingComponents/Videos/index.jsx
+++ src/front/src/components/LandingComponents/Videos/index.jsx
@@ -1,15 +1,33 @@
 import { VideoContainer} from "./styles"
+import "plyr/dist/plyr.css";
+import React, {useEffect, useRef} from "react";
+import Plyr from "plyr";
 
 const Video = () => {
-    return(
-        <VideoContainer>
-            
-        </VideoContainer>
-        
-    )
+  const videoRef = useRef(null);
 
-} 
+  useEffect(() => {
+    if (videoRef.current) {
+      const player = new Plyr(videoRef.current, {
+        controls: ["play", "progress", "current-time", "mute", "volume", "fullscreen"],
+      });
 
-export default Video
+      // Limpando instância quando o componente desmonta
+      return () => {
+        player.destroy();
+      };
+    }
+  }, []);
+
+  return (
+    <VideoContainer>
+      <video ref={videoRef} className="plyr" playsInline controls>
+        <source src="public/demo.mp4" type="video/mp4" />
+      </video>
+    </VideoContainer>
+  );
+};
+
+export default Video;
 
 
--- src/front/src/components/LandingComponents/Videos/styles.js
+++ src/front/src/components/LandingComponents/Videos/styles.js
@@ -6,12 +6,19 @@ export const VideoContainer = styled.div`
   left: 100px; /* dá espaço da linha até o vídeo */
   background-color: #fff;
   border-radius: 20px;
-  padding: 2rem;
+  padding: 10px;
   width: 960px;
-  height: 600px;
+  height: 550px;
   box-shadow: 0 15px 30px rgba(0, 0, 0, 0.1);
   z-index: 2;
   margin-left: 200px;
   margin-top: 200px;
+
+    video {
+    width: 100%;
+    border-radius: 12px;
+    outline: none;
+  }
+
 `
 
