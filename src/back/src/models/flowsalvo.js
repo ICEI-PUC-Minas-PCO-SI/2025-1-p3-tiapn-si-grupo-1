59e4084a9eeaefb6bbb0105561e71b3a46a98f14
RogerDLima <rogeriolimadias@gmail.com>
2025-06-04 17:12:06 -0300

    Flow salvo Model

--- src/back/src/models/flowsalvo.js
+++ src/back/src/models/flowsalvo.js
@@ -1,9 +1,24 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../../db');
 
-const FlowSalvo = sequelize.define('flowsalvo', {}, {
-  timestamps: false,
+const FlowSalvo = sequelize.define('flowsalvo', {
+  usuario_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: { model: 'usuario', key: 'id' }
+  },
+  flow_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: { model: 'flow', key: 'id' }
+  },
+  criado_em: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
   tableName: 'flow_salvo',
+  timestamps: false
 });
 
 
