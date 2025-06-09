e82e51a3b62e05e9433b148bd50ffe4b5b042203
RogerDLima <rogeriolimadias@gmail.com>
2025-06-09 12:14:17 -0300

    Auth Fix

--- src/back/src/middlewares/auth.js
+++ src/back/src/middlewares/auth.js
@@ -6,7 +6,8 @@ module.exports = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.usuarioId = decoded.id;
+    
+    req.usuario = { id: decoded.id };
     next();
   } catch {
     return res.status(403).json({ erro: 'Token inválido' });
