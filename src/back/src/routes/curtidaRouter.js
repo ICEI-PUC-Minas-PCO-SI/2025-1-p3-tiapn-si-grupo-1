f1e34a56d30bb1c9f68ebd91122f4e9013c502b9
FelipeRRoque <felipe.roque@sga.pucminas.br>
2025-06-19 20:29:24 -0300

    mudança nas rotas de curtida

--- src/back/src/routes/curtidaRouter.js
+++ src/back/src/routes/curtidaRouter.js
@@ -7,9 +7,11 @@ router.use(autenticarToken);
 
 router.post('/', curtidaController.criar);
 router.get('/', curtidaController.listar);
-router.get('/flow/:flow_id', curtidaController.listarCurtidasPorFlow);
-router.get('/:usuario_id/:flow_id', curtidaController.obter);
+router.get('/por-flow/:flow_id', curtidaController.listarCurtidasPorFlow);
+router.get('/verificar/:usuario_id/:flow_id', curtidaController.obter);
 router.delete('/:usuario_id/:flow_id', curtidaController.remover);
 
 
 module.exports = router;
+
+
