305bea5bcaa859091be5ff5c6acfb78993ef4f99
RogerDLima <rogeriolimadias@gmail.com>
2025-06-22 18:25:35 -0300

    Index Rework

--- src/back/src/models/index.js
+++ src/back/src/models/index.js
@@ -17,30 +17,25 @@ const models = {
   ComentarioPostagem,
 };
 
-// Flow x Usuario
 Usuario.hasMany(Flow, { foreignKey: "criado_por" });
 Flow.belongsTo(Usuario, { foreignKey: "criado_por" });
 
-// Comentario x Flow x Usuario
 Usuario.hasMany(Comentario, { foreignKey: "usuario_id" });
 Flow.hasMany(Comentario, { foreignKey: "flow_id" });
 Comentario.belongsTo(Usuario, { foreignKey: "usuario_id" });
 Comentario.belongsTo(Flow, { foreignKey: "flow_id" });
 
-// Curtidas para Flow
 Usuario.hasMany(Curtida, { foreignKey: "usuario_id", as: "curtidas" });
 Flow.hasMany(Curtida, { foreignKey: "flow_id", as: "curtidas" });
 Curtida.belongsTo(Usuario, { foreignKey: "usuario_id", as: "usuario" });
 Curtida.belongsTo(Flow, { foreignKey: "flow_id", as: "flow" });
 
-// Curtidas para Postagens
 PostagemComunidade.hasMany(Curtida, { foreignKey: "post_id", as: "curtidas" });
 Curtida.belongsTo(PostagemComunidade, {
   foreignKey: "post_id",
   as: "postagem",
 });
 
-// FlowSalvo
 Usuario.hasMany(FlowSalvo, {
   foreignKey: "usuario_id",
   as: "registrosFlowSalvo",
@@ -52,7 +47,6 @@ Flow.hasMany(FlowSalvo, {
 FlowSalvo.belongsTo(Usuario, { foreignKey: "usuario_id", as: "usuario" });
 FlowSalvo.belongsTo(Flow, { foreignKey: "flow_id", as: "flow" });
 
-// FlowSalvo também via belongsToMany (para busca simplificada)
 Usuario.belongsToMany(Flow, {
   through: FlowSalvo,
   as: "flowsSalvos",
@@ -64,25 +58,9 @@ Flow.belongsToMany(Usuario, {
   foreignKey: "flow_id",
 });
 
-// PostagemComunidade x Usuario
 Usuario.hasMany(PostagemComunidade, { foreignKey: "criado_por" });
 PostagemComunidade.belongsTo(Usuario, { foreignKey: "criado_por", as: "criador" });
 
-
-// ComentarioPostagem
-Usuario.hasMany(ComentarioPostagem, { foreignKey: "usuario_id" });
-PostagemComunidade.hasMany(ComentarioPostagem, {
-  foreignKey: "postagem_id",
-});
-
-ComentarioPostagem.belongsTo(Usuario, { foreignKey: "usuario_id", as: "usuario" });
-
-ComentarioPostagem.belongsTo(PostagemComunidade, {
-  as: "postagem",
-  foreignKey: "postagem_id",
-});
-
-// Se algum model tiver método associate, chama
 Object.values(models).forEach((model) => {
   if (model.associate) {
     model.associate(models);
